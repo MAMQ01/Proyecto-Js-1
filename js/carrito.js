d5af3e05685cc50ba5dbe787f2fb5f3071dbdeb3
MAMQ01 <andresmq2018@gmail.com>
2023-04-28 16:51:53 +0200

    Ajustando los decimales del total

--- js/carrito.js
+++ js/carrito.js
@@ -125,7 +125,7 @@ function vaciarCarrito() {
 
 function actualizarTotal() {
     const totalCalculado = productosEnCarrito.reduce((acc, producto) => acc + (producto.precio * producto.cantidad), 0);
-    total.innerText = `$${totalCalculado}`;
+    total.innerText = `$${totalCalculado.toFixed(2)}`;
 }
 
 botonComprar.addEventListener("click", comprarCarrito);
